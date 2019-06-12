d08c5d33a775ed28ad32427fdf7786c2903442bd
krikaz <pascal.bou@protonmail.com>
2019-06-12 16:45:17 +0200

    added PUT method if id if supplied

--- friends/src/components/AddNewFriend.jsx
+++ friends/src/components/AddNewFriend.jsx
@@ -3,52 +3,100 @@ import axios from 'axios';
 
 export default class AddNewFriend extends React.Component {
 	state = {
+		id: 0,
 		name: '',
 		age: 0,
 		email: '',
 	};
 
 	addFriend = () => {
-    if (this.state.name && this.state.age && this.state.email) {
-      axios
-        .post('http://localhost:5000/friends', {
-          name: this.state.name,
-          age: this.state.age,
-          email: this.state.email,
-        })
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
-    }
+		if (this.state.name && this.state.age && this.state.email) {
+			axios
+				.post('http://localhost:5000/friends', {
+					name: this.state.name,
+					age: this.state.age,
+					email: this.state.email,
+				})
+				.then(response => console.log(response))
+				.catch(error => console.log(error));
+		}
+	};
+
+	updateFriend = () => {
+		let link = 'http://localhost:5000/friends/' + this.state.id;
+		if (this.state.name && this.state.age && this.state.email) {
+			axios
+				.put(link, {
+					name: this.state.name,
+					age: this.state.age,
+					email: this.state.email,
+				})
+				.then(response => console.log(response))
+				.catch(error => console.log(error));
+		}
+  };
+  
+  deleteFriend = () => {
+		let link = 'http://localhost:5000/friends/' + this.state.id;
+		if (this.state.id) {
+			axios
+				.delete(link)
+				.then(response => console.log(response))
+				.catch(error => console.log(error));
+		}
+  }
+
+	handleSubmit = () => {
+		this.state.id ? this.updateFriend() : this.addFriend();
 	};
 
 	handleChange = e => {
-    this.setState({ [e.target.name]: e.target.value });
-    // console.log(this.state);
+		this.setState({ [e.target.name]: e.target.value });
+		// console.log(this.state);
 	};
 
 	render() {
 		return (
-			<form onSubmit={this.addFriend}>
-				<input
-					name="name"
-					type="text"
-					placeholder="name"
-					onChange={this.handleChange}
-				/>
-				<input
-					name="age"
-					type="text"
-					placeholder="age"
-					onChange={this.handleChange}
-				/>
-				<input
-					name="email"
-					type="text"
-					placeholder="email"
-					onChange={this.handleChange}
-				/>
-				<button type="submit">Add Friend</button>
-			</form>
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<input
+						name="id"
+						type="number"
+						placeholder="id for updating"
+						onChange={this.handleChange}
+					/>
+
+					<input
+						name="name"
+						type="text"
+						placeholder="name"
+						onChange={this.handleChange}
+					/>
+					<input
+						name="age"
+						type="number"
+						placeholder="age"
+						onChange={this.handleChange}
+					/>
+					<input
+						name="email"
+						type="email"
+						placeholder="email"
+						onChange={this.handleChange}
+					/>
+					<button type="submit">Submit Change</button>
+				</form>
+
+				<form onSubmit={this.deleteFriend}>
+					<input
+						name="id"
+						type="number"
+						placeholder="id to delete"
+						onChange={this.handleChange}
+					/>
+					<button type="submit">Delete Friend</button>
+				</form>
+			</div>
 		);
 	}
 }
