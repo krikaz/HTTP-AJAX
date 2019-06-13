7834f716f91bc5a56a51f437f631f28aca285f15
krikaz <pascal.bou@protonmail.com>
2019-06-13 13:58:53 +0200

    created form to add or update a friend

--- friends/src/components/Friends.jsx
+++ friends/src/components/Friends.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import axios from 'axios';
 import Friend from './Friend';
+import styled from 'styled-components';
 
 const friendsAPI = 'http://localhost:5000/friends';
+
+const Form = styled.div`
+	border: 1px solid black;
+	margin: 1rem;
+	padding: 1rem;
+`;
 
 export default class AddNewFriend extends React.Component {
 	state = {
@@ -11,8 +18,13 @@ export default class AddNewFriend extends React.Component {
 		friend: null,
 	};
 
+	idToUpdate = React.createRef();
+	nameToUpdate = React.createRef();
+	ageToUpdate = React.createRef();
+	emailToUpdate = React.createRef();
+
 	componentDidMount() {
-		// this.getAllFriends();
+		this.getAllFriends();
 	}
 
 	getAllFriends = () => {
@@ -21,9 +33,29 @@ export default class AddNewFriend extends React.Component {
 	};
 
 	addFriend = () => {
+		const newFriend = {
+			name: this.nameToUpdate.current.value,
+			age: this.ageToUpdate.current.value,
+			email: this.emailToUpdate.current.value,
+		}
+		axios.post(friendsAPI, newFriend)
+			.then(() => this.getAllFriends());
 	};
 
 	updateFriend = () => {
+		const newFriend = {
+			name: this.nameToUpdate.current.value,
+			age: this.ageToUpdate.current.value,
+			email: this.emailToUpdate.current.value,
+		}
+		axios.put(`${friendsAPI}/${this.idToUpdate.current.value}`, newFriend)
+			.then(() => this.getAllFriends());
+	};
+
+	modifyFriend = () => {
+		this.idToUpdate.current.value
+		? this.updateFriend()
+		: this.addFriend()
 	};
 
 	deleteFriend = () => {
@@ -32,12 +64,20 @@ export default class AddNewFriend extends React.Component {
 	render() {
 		return (
 			<div>
-				<div>
+				<Form>
 					{this.state.friends.map(fr => (
 						<Friend key={fr.id} friend={fr} />
 					))}
 					<button onClick={this.getAllFriends}>getAllFriends</button>
-				</div>
+				</Form>
+
+				<Form>
+						<input type='text' placeholder='id to update' ref={this.idToUpdate} />
+						<input type='text' placeholder='name' ref={this.nameToUpdate} />
+						<input type='number' placeholder='age' ref={this.ageToUpdate} />
+						<input type='text' placeholder='email' ref={this.emailToUpdate} />
+						<button onClick={this.modifyFriend}>submit changes</button>
+				</Form>
 
 			</div>
 		);
